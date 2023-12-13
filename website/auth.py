48153ada2b9675a07b4897a4f436ab83b4b79855
MyPetLobster <6979547+MyPetLobster@users.noreply.github.com>
2023-12-12 22:52:32 -0500

    Implement db and auth, SQLAlchemy bug

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,91 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import db
+from .models import User
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash   
+
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/register')
+@auth.route('/register', methods=['POST', 'GET'])
 def register():
+    if request.method == 'POST':
+        print(request.form)
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('psw')
+        password2 = request.form.get('psw-repeat')
+
+        email = User.query.filter_by(email=email).first()
+        username = User.query.filter_by(username=username).first()
+
+        print(f'{email}, {username}, {password}, {password2}')
+        print("**************************")
+
+        if email:
+            flash('Email already exists.', category='error')
+        elif username:
+            flash('Username already exists.', category='error')
+        elif password != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(username) < 3:
+            flash('Username must be at least 3 characters.', category='error')
+        elif len(password) < 6:
+            flash('Password must be at least 6 characters.', category='error')
+        elif password != password2:
+            flash('Passwords do not match.', category='error')
+        else:
+            new_user = User(email=email, username=username, password=generate_password_hash(password, method='sha256'))
+            # add the variable that stores the new user (User object) to the database
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!', category='success')
+            login_user(new_user, remember=True)
+            return redirect(url_for('views.home'))
+        
     return render_template("register.html")
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        username = request.form.get('usrname')
+        password = request.form.get('psw')
+
+        user = User.query.filter_by(username=username).first()
+        if user: 
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+
+
 
 @auth.route('/logout')  
+@login_required
 def logout():
     return redirect(url_for('views.home'))
+
+
+
+
+
+
+# def login():
+#     if request.method == 'POST':
+#         username = request.form.get('usrname')
+#         password = request.form.get('psw')
+#         remember = True if request.form.get('remember') else False
+
+#         user = User.query.filter_by(username=username).first()
+
+#         if not user or not check_password_hash(user.password, password):
+#             flash('Please check your login details and try again.', category='error')
+#         else:
+#             login_user(user, remember=remember)
+#             flash('Logged in successfully!', category='success')
+#             return redirect(url_for('views.home'))
+    
+#     return render_template("login.html")
