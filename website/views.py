b85c951ca9014dceab761560e02f3cd4717a3a5a
MyPetLobster <6979547+MyPetLobster@users.noreply.github.com>
2023-12-13 11:42:31 -0500

    Pass current_user to all views

--- website/views.py
+++ website/views.py
@@ -15,35 +15,27 @@ def home():
 @login_required
 def profile():
     username = current_user.username if current_user.is_authenticated else None
-    return render_template("profile.html", username=username)
+    return render_template("profile.html", username=username, user=current_user)
 
 @views.route('/bookcases/')
 @login_required
 def bookcases():
-    return render_template("bookcases.html")
+    return render_template("bookcases.html", user=current_user)
 
 @views.route('/bookcase/<int:id>/')
 @login_required
 def bookcase(id):
-    return render_template("bookcase.html", id=id)
+    return render_template("bookcase.html", id=id, user=current_user)
 
 @views.route('/book/<int:id>/')
 @login_required
 def book(id):
-    return render_template("book.html", id=id)
+    return render_template("book.html", id=id, user=current_user)
 
 
 
 
 ##### NO LOGIN REQUIRED #####
-@views.route('/signup/')
-def signup():
-    return render_template("signup.html")
-
-@views.route('/login/')
-def login():
-    return render_template("login.html")    
-
 @views.route('/about/')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", user=current_user)
